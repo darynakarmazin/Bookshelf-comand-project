22708bbd86f3039762e30a028471eb222e066ce0
Volodymyr Bondar <2005volka@gmail.com>
2023-05-05 10:25:56 +0300

    add base

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/header/header-burger';
+
+import './js/categories/categories';
+import './js/support-ukraine/support-ukraine';
+import './js/best-sellers/best-sellers';
+import './js/book-modal/book-modal';
+import './js/shopping-list/shopping-list';
+import './js/authorization/authorization';
