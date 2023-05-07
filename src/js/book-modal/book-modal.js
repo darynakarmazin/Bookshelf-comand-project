e6a56f2f8993b898aa29118a601212a0b599bea3
Bogdddan <bogdankrajnik95@gmail.com>
2023-05-07 18:33:11 +0300

    modal for book , without modal body

--- src/js/book-modal/book-modal.js
+++ src/js/book-modal/book-modal.js
@@ -1,11 +1,50 @@
-const openModalBtn = document.querySelector("[data-modal-open]");
-const closeModalBtn = document.querySelector("[data-modal-close]");
-const modal = document.querySelector("[data-modal]");
-
-openModalBtn.addEventListener("click", () => {
-modal.classList.toggle("is-hidden");
-});
-
-closeModalBtn.addEventListener("click", () => {
-modal.classList.toggle("is-hidden");
-});
+window.onload = function() {
+    const ulBooksList = document.querySelectorAll('.books-list');
+  
+    ulBooksList.forEach(book => {
+      book.addEventListener('click', (event) => {
+        event.preventDefault();
+  
+        // Знайдіть модальне вікно та елементи з інформацією про книгу
+        const modal = document.querySelector('.modal');
+        const title = modal.querySelector('.book-title');
+        const author = modal.querySelector('.book-author');
+        const description = modal.querySelector('.book-description');
+  
+        const bookLink = event.target.closest('.books-list-img');
+        if (!bookLink) return;
+        const bookId = bookLink.dataset.id;
+  
+        modal.classList.add('modal-active');
+  
+        fetch(`https://books-backend.p.goit.global/books/${bookId}`)
+          .then(response => response.json())
+          .then(data => {
+            title.textContent = data.title;
+            author.textContent = data.author;
+            description.textContent = data.description;
+          })
+          .catch(error => console.error(error));
+      });
+    });
+  
+    // Закриття модального вікна при натисканні на кнопку закриття або за межами вікна
+    const modal = document.querySelector('[data-modal]');
+    const closeButton = document.querySelector('[data-modal-close]');
+    const modalBackground = document.querySelector('.modal');
+  
+    modalBackground.addEventListener('click', (event) => {
+      if (event.target === modalBackground) {
+        closeModal();
+      }
+    });
+  
+    closeButton.addEventListener('click', () => {
+      closeModal();
+    });
+  
+    function closeModal() {
+      modalBackground.classList.remove('modal-active');
+    }
+  }
+  
