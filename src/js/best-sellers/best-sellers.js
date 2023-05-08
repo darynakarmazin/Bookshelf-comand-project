46294d4e24aba1d84915f229af0454ce47c90d39
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-05-08 12:05:16 +0300

    Added function to categories btn

--- src/js/best-sellers/best-sellers.js
+++ src/js/best-sellers/best-sellers.js
@@ -1,33 +1,106 @@
-
-import { fetchTopBooks } from "./fetchTopBooks";
-
-
-fetchTopBooks().then(dataMarkupAll).catch();
+import { fetchTopBooks } from './fetchTopBooks';
+import { onFiltred } from '../categories/categories';
 
 const ulBooksList = document.querySelector('.books-list');
-const divBooksList = document.querySelector('.books-list-wrapper');
+const divBooksList = document.querySelector('.books-list-title');
 
-function dataMarkupAll(data) {
-  const dataMarkupAll = data
+onRenderBestsellers();
+
+export function onRenderBestsellers() {
+  fetchTopBooks().then(dataBestsellers).catch();
+}
+
+function dataBestsellers(data) {
+  const dataBestsellers = data
     .map(elem => {
-      console.log(elem);
-      for (let i = 0; i <= 5; i++)
-        return `
-        <li class="caterory"><h2 class="category-title">${elem.list_name}</h2>
-        <ul class="category-top-books">
-        <a href=""><li><img class="best-img" src="${elem.books[0].book_image}" width='180px' height='256px' alt="${elem.books[0].title}"><h3 class = "book-name" >${elem.books[0].title}</h3><p class= "book-author">${elem.books[0].author}</p></li></a>
-        <a href=""><li><img class="best-img" src="${elem.books[1].book_image}" width='180px' height='256px' alt="${elem.books[1].title}"><h3 class = "book-name" >${elem.books[1].title}</h3><p class= "book-author">${elem.books[1].author}</p></li></a>
-        <a href=""><li><img class="best-img" src="${elem.books[2].book_image}" width='180px' height='256px' alt="${elem.books[2].title}"><h3 class = "book-name" >${elem.books[2].title}</h3><p class= "book-author">${elem.books[2].author}</p></li></a>
-        <a href=""><li><img class="best-img" src="${elem.books[3].book_image}" width='180px' height='256px' alt="${elem.books[3].title}"><h3 class = "book-name" >${elem.books[3].title}</h3><p class= "book-author">${elem.books[3].author}</p></li></a>
-        <a href=""><li><img class="best-img" src="${elem.books[4].book_image}" width='180px' height='256px' alt="${elem.books[4].title}"><h3 class = "book-name" >${elem.books[4].title}</h3><p class= "book-author">${elem.books[4].author}</p></li></a>
-        </ul> 
-        <button data-filter="${elem.list_name}" class="list-name list-name-btn">see more</button>
+      return `
+  <li class="caterory"><h2 class="category-title">${elem.list_name}</h2>
+    <ul class="category-top-books">
+      <li>
+        <a class="books-list-link" href="">
+        <div class="thumb">
+          <img class="books-list-img" src="${elem.books[0].book_image}" alt="${elem.books[0].title}">
+          <div class="actions-card">
+            <p class="discription">quick view</p>
+          </div>
+        </div>
+          <div class="content">
+            <h3 class="books-list-name">${elem.books[0].title}</h3>
+            <p class="books-list-text">${elem.books[0].author}</p> 
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="books-list-link" href="">
+        <div class="thumb">
+          <img class="books-list-img" src="${elem.books[1].book_image}" alt="${elem.books[1].title}">
+          <div class="actions-card">
+            <p class="discription">quick view</p>
+          </div>
+        </div>
+          <div class="content">
+            <h3 class="books-list-name">${elem.books[1].title}</h3>
+            <p class="books-list-text">${elem.books[1].author}</p> 
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="books-list-link" href="">
+        <div class="thumb">
+          <img class="books-list-img" src="${elem.books[2].book_image}" alt="${elem.books[2].title}">
+          <div class="actions-card">
+            <p class="discription">quick view</p>
+          </div>
+        </div>
+          <div class="content">
+            <h3 class="books-list-name">${elem.books[2].title}</h3>
+            <p class="books-list-text">${elem.books[2].author}</p> 
+          </div>
+        </a>
+      </li>
+      <li>
+        <a class="books-list-link" href="">
+        <div class="thumb">
+          <img class="books-list-img" src="${elem.books[3].book_image}" alt="${elem.books[3].title}">
+          <div class="actions-card">
+            <p class="discription">quick view</p>
+          </div>
+        </div>
+          <div class="content">
+            <h3 class="books-list-name">${elem.books[3].title}</h3>
+            <p class="books-list-text">${elem.books[3].author}</p> 
+          </div>
+        </a>
+      </li>
+            <li>
+        <a class="books-list-link" href="">
+        <div class="thumb">
+          <img class="books-list-img" src="${elem.books[4].book_image}" alt="${elem.books[4].title}">
+          <div class="actions-card">
+            <p class="discription">quick view</p>
+          </div>
+        </div>
+          <div class="content">
+            <h3 class="books-list-name">${elem.books[4].title}</h3>
+            <p class="books-list-text">${elem.books[4].author}</p> 
+          </div>
+        </a>
+      </li>
+        <button data-filter="${elem.list_name}" class="list-name best-sellers-btn">see more</button>
         </li>`;
     })
     .join(' ');
-  ulBooksList.innerHTML = dataMarkupAll;
+  ulBooksList.innerHTML = dataBestsellers;
 
-  const dataMarkupTitle = `
-    <h1 class="best-title" >Best Sellers <span class="best-title-span">Books</span></h1>`;
+  const dataMarkupTitle = `<h2>Best Sellers <span>Books</span></h2>`;
   divBooksList.innerHTML = dataMarkupTitle;
+
+  oncategorriesBtn();
+}
+
+function oncategorriesBtn() {
+  const categorriesBtn = document.querySelectorAll('.best-sellers-btn');
+  categorriesBtn.forEach(element =>
+    element.addEventListener('click', onFiltred)
+  );
 }
