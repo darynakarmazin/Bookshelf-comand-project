f5c38d822e4b7dd5f9db6456f54fc43e3a89b8ab
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-05-08 19:48:16 +0300

    support-Ukraine-08052023

---
+++ src/js/support-ukraine/support-ukraine-array.js
@@ -0,0 +1,56 @@
+export const array = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: new URL('../../images/book-icon/save.png', import.meta.url),
+    img2: new URL('../../images/book-icon/save@2x.png', import.meta.url),
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: new URL('../../images/book-icon/hope.png', import.meta.url),
+    img2: new URL('../../images/book-icon/hope@2x.png', import.meta.url),
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: new URL('../../images/book-icon/medical.png', import.meta.url),
+    img2: new URL('../../images/book-icon/medical@2x.png', import.meta.url),
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: new URL('../../images/book-icon/razom.png', import.meta.url),
+    img2: new URL('../../images/book-icon/razom@2x.png', import.meta.url),
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: new URL('../../images/book-icon/action.png', import.meta.url),
+    img2: new URL('../../images/book-icon/action@2x.png', import.meta.url),
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: new URL('../../images/book-icon/prytula.png', import.meta.url),
+    img2: new URL('../../images/book-icon/prytula@2x.png', import.meta.url),
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: new URL('../../images/book-icon/medecins.png', import.meta.url),
+    img2: new URL('../../images/book-icon/medecins@2x.png', import.meta.url),
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: new URL('../../images/book-icon/world.png', import.meta.url),
+    img2: new URL('../../images/book-icon/world@2x.png', import.meta.url),
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: new URL('../../images/book-icon/united.png', import.meta.url),
+    img2: new URL('../../images/book-icon/united@2x.png', import.meta.url),
+  },
+];
---
+++ src/js/support-ukraine/support-ukraine.js
@@ -0,0 +1,102 @@
+import { array } from './support-ukraine-array';
+
+const container = document.querySelector('#support-ukraine');
+const loadMoreBtn = document.querySelector('button.button-support-ukraine');
+let limit = 0;
+let inc = 2;
+
+(function () {
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  console.log(vw);
+  if (vw < 768) {
+    limit = 4;
+    renderListSupport(array, limit);
+  } else {
+    limit = 6;
+    renderListSupport(array, limit);
+  }
+})();
+
+function renderListSupport(array, limit) {
+  clearContainer();
+  const markup = array
+    .map(({ title, url, img, img2 }, index) => {
+      index += 1;
+      if (index < limit + 1) {
+        return `        
+          <div class="logo-support-ukraine">
+            0${index}<a
+              href="${url}"
+              title="${title}"
+              target="_blank"
+              rel="noopener noreferrer"
+              ><img
+                srcset="${img} 1x, ${img2} 2x"
+                src="${img}"
+                alt="${title}"
+                height="32"
+            /></a>
+            </div>
+  `;
+      }
+    })
+    .join('');
+  container.insertAdjacentHTML('beforeend', markup);
+}
+
+loadMoreBtn.addEventListener('click', scrolList);
+
+function scrolList() {
+  clearContainer();
+  if (limit === 6) {
+    if (inc < limit - 1) {
+      renderListScrol(array, limit, inc);
+      inc += 1;
+    } else {
+      renderListSupport(array, limit);
+      inc = 2;
+      return;
+    }
+  } else if (inc < limit + 3) {
+    renderListScrol(array, limit, inc);
+    inc += 1;
+  } else {
+    renderListSupport(array, limit);
+    inc = 2;
+    return;
+  }
+}
+
+function renderListScrol(array, limit, inc) {
+  console.log('inc', inc);
+  const markup = array
+    .map(({ title, url, img, img2 }, index) => {
+      index += 1;
+      if (index >= inc && index < limit + inc) {
+        return `        
+          <div class="logo-support-ukraine">
+            0${index}<a
+              href="${url}"
+              title="${title}"
+              target="_blank"
+              rel="noopener noreferrer"
+              ><img
+                srcset="${img} 1x, ${img2} 2x"
+                src="${img}"
+                alt="${title}"
+                height="32"
+            /></a>
+            </div>
+  `;
+      }
+    })
+    .join('');
+  container.innerHTML = markup;
+}
+
+function clearContainer() {
+  container.innerHTML = '';
+}
